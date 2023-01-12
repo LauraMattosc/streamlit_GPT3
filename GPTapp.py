dab0a07a3c8b89e778d4abf50d4e546956e31033
Laura Mattos <89162142+LauraMattosc@users.noreply.github.com>
2023-01-12 20:14:27 -0300

    Update GPTapp.py

--- GPTapp.py
+++ GPTapp.py
@@ -1,26 +1,37 @@
 import streamlit as st
+import requests
 import pandas as pd
 import matplotlib.pyplot as plt
 
+# Define the API endpoint and parameters
+api_key = "1036d77762adc57b8c2dddcb299ad2f9"
+endpoint = "http://api.openweathermap.org/data/2.5/weather"
+params = {"q": "Sao Paulo,BR", "appid": api_key, "units": "metric"}
 
-# Create some fields for user input
-name = st.text_input("Enter your name")
-age = st.number_input("Enter your age")
+st.title("Sao Paulo Weather")
+
+# Fetch the current weather data from the API
+response = requests.get(endpoint, params=params)
+data = response.json()
+
+# Extract the current temperature from the data
+temp = data["main"]["temp"]
+st.write(f"The current temperature in Sao Paulo is {temp}°C")
 
 # Create a checkbox to show/hide the graph
-show_graph = st.checkbox("Show graph")
+show_graph = st.checkbox("Show temperature over time")
 
 if show_graph:
-    # Create a sample dataset
-    df = pd.DataFrame({
-        "x": [1, 2, 3, 4],
-        "y": [10, 15, 13, 17]
-    })
+    # Fetch the historical weather data from the API
+    endpoint = "http://api.openweathermap.org/data/2.5/forecast"
+    response = requests.get(endpoint, params=params)
+    data = response.json()
 
-    # Create a line plot
-    plt.plot(df["x"], df["y"])
+    # Extract the temperature data from the response
+    temp_data = [item["main"]["temp"] for item in data["list"]]
+
+    # Create a line plot of the temperature over time
+    plt.plot(temp_data)
+    plt.xlabel("Time")
+    plt.ylabel("Temperature (°C)")
     st.pyplot()
-
-# Display user input
-st.write("Your name is", name)
-st.write("Your age is", age)
